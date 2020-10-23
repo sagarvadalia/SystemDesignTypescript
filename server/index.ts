e25b537f50cb2da80a76265030ce1c5e5ed69161
sagarvadalia <sv344@cornell.edu>
2020-10-23 10:58:47 -0400

    automated linting

--- server/index.ts
+++ server/index.ts
@@ -51,7 +51,7 @@ createConnection()
 
 		// register express routes from defined application routes
 		Routes.forEach((route) => {
-			(app as any)[route.method](route.route, (req: Request, res: Response, next) => {
+			(app as any)[route.method](route.route, (req: Request, res: Response, next: Response) => {
 				const result = new (route.controller as any)()[route.action](req, res, next);
 				if (result instanceof Promise) {
 					result.then((result) => (result !== null && result !== undefined ? res.send(result) : undefined));
