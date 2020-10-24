187ecd79a52acaed575075ec69fbcbd4c139ff63
Tyler Heinsohn <tjheinzo@gmail.com>
2020-10-24 19:35:47 -0400

    Update Lab.ts

--- server/entity/Lab.ts
+++ server/entity/Lab.ts
@@ -5,7 +5,6 @@ import { Graduate } from './Graduate';
 import { Room } from './Room';
 import { UnderGraduate } from './UnderGraduate';
 
-// Need to handle the association with course
 @Entity()
 export class Lab extends Room {
 	constructor(...args) {
