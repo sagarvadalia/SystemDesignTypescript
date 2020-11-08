3e29d30744cf90ec975353f9122759e308d282f5
sagarvadalia <sv344@cornell.edu>
2020-11-08 11:44:00 -0500

    reformated seeds to not crash when they fail. Also seeded undergraduate part time

--- server/seed/index.ts
+++ server/seed/index.ts
@@ -3,5 +3,13 @@ import undergraduatePartTime = require('./undergraduatePartTime');
 import partTimeFaculty = require('./partTimeFaculty');
 import fullTimeFaculty = require('./fullTimeFaculty');
 import graduateFullTime = require('./graduateFullTime');
+import undergraduateFullTime = require('./undergraduateFullTime');
 
-export const seeds = { partTimeFaculty, fullTimeFaculty, graduateFullTime, undergraduatePartTime, graduatePartTime };
+export const seeds = {
+	partTimeFaculty,
+	fullTimeFaculty,
+	graduateFullTime,
+	undergraduatePartTime,
+	graduatePartTime,
+	undergraduateFullTime,
+};
