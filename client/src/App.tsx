2656f17a60135d6c907a7f3e92b00a9718a62eb9
sagarvadalia <sv344@cornell.edu>
2020-11-19 17:42:09 -0500

    data fetching

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,21 +2,25 @@ import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import { Users } from '../server/entity/Users/Users';
 function App() {
-  const [data, setData] = useState({});
+  const [data, setData] = useState({ users: Array<Users>() });
 
   useEffect(() => {
     const fetchData = async () => {
       const result = await axios(
         '/users',
       );
-
-      setData(result.data);
-      console.log(result)
+      const data: Array<Users> = result.data
+      setData({ users: data });
+      console.log(data)
     };
 
     fetchData();
+    console.log(data);
+    console.log(data.users[0].userID)
   }, []);
+
   return (
     <div className="App">
       <header className="App-header">
