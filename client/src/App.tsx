08df25a99922cb54abf460fb6931d56045f318ed
sagarvadalia <sv344@cornell.edu>
2020-11-23 18:26:44 -0500

    schedule demo added and showing all users

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,22 +3,25 @@ import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 import { Users } from '../server/entity/Users/Users';
+import Schedule from './client/components/schedule';
 function App() {
   const [data, setData] = useState({ users: Array<Users>() });
 
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios(
-        '/users',
-      );
-      const data: Array<Users> = result.data
-      setData({ users: data });
-      console.log(data)
+      try {
+        const result = await axios.get('/users/');
+        const data: Array<Users> = result.data
+        setData({ users: data });
+        console.log(data)
+      } catch (error) {
+        console.error(error);
+      }
     };
 
     fetchData();
     console.log(data);
-    console.log(data.users[0].userID)
+    console.log(data?.users[0]?.userID)
   }, []);
 
   return (
@@ -27,7 +30,16 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
+
+          <Schedule></Schedule>
         </p>
+
+        <pre>
+          <p >
+            {JSON.stringify(data)}
+          </p>
+
+        </pre>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ client/src/client/components/navbar.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export interface IAppProps {
+}
+
+export default function Navbar(props: IAppProps) {
+  return (
+    <div>
+
+    </div>
+  );
+}
