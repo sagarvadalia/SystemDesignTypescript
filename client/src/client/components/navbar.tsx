e19bdf28c092e5e4fcad20dce99edc8197081909
sagarvadalia <sv344@cornell.edu>
2020-11-24 15:56:22 -0500

    navbar

--- client/src/client/components/navbar.tsx
+++ client/src/client/components/navbar.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import PersistentDrawerLeft from './appbar';
+
 
 export interface IAppProps {
 }
@@ -6,7 +8,7 @@ export interface IAppProps {
 export default function Navbar(props: IAppProps) {
   return (
     <div>
-
+      <PersistentDrawerLeft></PersistentDrawerLeft>
     </div>
   );
 }
