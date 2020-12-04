a91f05590b5d988c7b0b32257451eaf2fb30d119
TJHeinzo <tjheinzo@gmail.com>
2020-12-04 15:29:06 -0500

    Added credit checking in addClass

--- guptauniversity/server/controller/JoinTables/StudentMinorController.ts
+++ guptauniversity/server/controller/JoinTables/StudentMinorController.ts
@@ -2,16 +2,18 @@ import { getRepository } from 'typeorm';
 import { NextFunction, Request, Response } from 'express';
 import { StudentMinor } from '../../entity/JoinTables/StudentMinor';
 import { validate, validateOrReject } from 'class-validator';
+import { Student } from '../../entity/Users/Student';
 
 export class StudentMinorController {
-	private StudentMinorRepository = getRepository(StudentMinor);
+	private studentMinorRepository = getRepository(StudentMinor);
+	private studentRepository = getRepository(Student);
 
 	async all(request: Request, response: Response, next: NextFunction) {
-		return this.StudentMinorRepository.find();
+		return this.studentMinorRepository.find();
 	}
 
 	async one(request: Request, response: Response, next: NextFunction) {
-		return this.StudentMinorRepository.findOne(request.params.id);
+		return this.studentMinorRepository.findOne(request.params.id);
 	}
 
 	async save(request: Request, response: Response, next: NextFunction) {
@@ -19,17 +21,17 @@ export class StudentMinorController {
 		const errors = await validate(studentMinor);
 		console.log(errors);
 		if (!errors) {
-			return this.StudentMinorRepository.save(request.body);
+			return this.studentMinorRepository.save(request.body);
 		}
 	}
 
 	async remove(request: Request, response: Response, next: NextFunction) {
-		const studentMinorToRemove: StudentMinor | undefined = await this.StudentMinorRepository.findOne(
+		const studentMinorToRemove: StudentMinor | undefined = await this.studentMinorRepository.findOne(
 			request.params.id,
 		);
 		try {
 			if (studentMinorToRemove) {
-				await this.StudentMinorRepository.remove(studentMinorToRemove);
+				await this.studentMinorRepository.remove(studentMinorToRemove);
 			}
 		} catch (error) {
 			console.error(error);
