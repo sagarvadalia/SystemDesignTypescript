f7cc85f9a5fa530eebe055a432c9f2445cfaa99b
sagarvadalia <sv344@cornell.edu>
2020-12-17 22:46:18 -0500

    prereq dets

--- guptauniversity/src/components/Utils/CourseDetails.jsx
+++ guptauniversity/src/components/Utils/CourseDetails.jsx
@@ -3,11 +3,19 @@ import MaterialTable from 'material-table';
 import axios from 'axios';
 import { LoginContext } from './../../LoginContext';
 import { Link, useParams } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+import history from './../../history';
 export default function CourseDetails() {
 	const [data, setData] = useState([{ deptID: {} }]);
 	const [state, setState] = useContext(LoginContext);
 	let { courseID } = useParams();
 	courseID = parseInt(courseID);
+	async function showPrereq(prereqID) {
+		history.push(`/courses/${prereqID}`);
+		const result = await axios(`/api/courses/${prereqID}`);
+		console.log(result.data);
+		setData(result.data);
+	}
 	useEffect(() => {
 		const fetchData = async () => {
 			console.log(courseID);
@@ -34,6 +42,18 @@ export default function CourseDetails() {
 						{ title: 'Course Description', field: 'courseDesc' },
 						{ title: 'Credits', field: 'numOfCredits' },
 						{ title: 'Department', field: 'deptID.deptName' },
+						{
+							title: 'Prerequisites',
+							render: (rowData) => (
+								<div>
+									{rowData?.prereqs?.map((prereq) => (
+										<Button onClick={() => showPrereq(prereq.prereqID)}>
+											{`CourseID: ${prereq.prereqID} Grade Required: \n ${prereq.gradeRequired} \n`}
+										</Button>
+									))}
+								</div>
+							),
+						},
 					]}
 					data={[data]}
 					options={{
